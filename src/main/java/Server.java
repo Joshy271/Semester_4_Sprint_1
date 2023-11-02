b5fcbdd41e33d56e4655926a46805ea6f417c452
Joshy <117656288+Joshy271@users.noreply.github.com>
2023-11-02 15:56:12 -0230

    added some dependancies

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class Server {
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        // Create an HTTP server that listens on port 8080
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        // Create a context for the "/api" path
+        server.createContext("/api", new ApiHandler());
+
+        // Start the server
+        server.start();
+
+        System.out.println("Server started on port 8080.");
+    }
+
+    static class ApiHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            // Handle incoming HTTP requests for the "/api" path
+            String response = "Hello, this is your REST API response.";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    @SpringBootApplication
+    public class ServerApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(ServerApplication.class, args);
+        }
+
+
+
+        private static void listAirportsInCity() {
+            // Implement the logic to list airports in a specific city
+        }
+
+        private static void listAircraftForPassenger() {
+            // Implement the logic to list aircraft for a specific passenger
+        }
+
+        private static void listAirportsForAircraft() {
+            // Implement the logic to list airports for a specific aircraft
+        }
+
+        private static void listAirportsForPassenger() {
+            // Implement the logic to list airports for a specific passenger
+        }
+
+        // ... Define your API endpoints ...
+    }
 }
